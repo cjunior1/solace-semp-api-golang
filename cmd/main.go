3a947c25075f384e572db4a4ea60d58377c7ddd5
carlos <cjunior1@gmail.com>
2023-01-17 09:38:57 -0300

    change package name

--- cmd/main.go
+++ cmd/main.go
@@ -6,23 +6,23 @@ import (
 	"net/url"
 	"os"
 
-	swagger "github.com/cjunior1/solace-semp-api-golang"
+	semp "github.com/cjunior1/solace-semp-api-golang"
 )
 
 func main() {
 
-	cfg := swagger.NewConfiguration()
-	client := swagger.NewAPIClient(cfg)
+	cfg := semp.NewConfiguration()
+	client := semp.NewAPIClient(cfg)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	// credenciais
-	credencial := &swagger.BasicAuth{}
+	credencial := &semp.BasicAuth{}
 	credencial.UserName = os.Getenv("SOLACE_USER_NAME")
 	credencial.Password = os.Getenv("SOLACE_PASSWORD")
 
-	ctx = context.WithValue(ctx, swagger.ContextBasicAuth, *credencial)
+	ctx = context.WithValue(ctx, semp.ContextBasicAuth, *credencial)
 
 	// estado atual
 	queueName := "xx-demo-producer-happy-path-dev"
@@ -34,7 +34,7 @@ func main() {
 		//fmt.Println(resp.Meta.ResponseCode)
 		//		if resp.Meta.Error_.Status == "NOT FOUND" {
 		fmt.Println("Topic Not found : ", subscriptionTopic)
-		novoTopic := swagger.MsgVpnQueueSubscription{MsgVpnName: serviceName, QueueName: queueName, SubscriptionTopic: subscriptionTopic}
+		novoTopic := semp.MsgVpnQueueSubscription{MsgVpnName: serviceName, QueueName: queueName, SubscriptionTopic: subscriptionTopic}
 		resp, _, err := client.MsgVpnApi.CreateMsgVpnQueueSubscription(ctx, serviceName, queueName, novoTopic, nil)
 		if err != nil {
 			panic(err)
